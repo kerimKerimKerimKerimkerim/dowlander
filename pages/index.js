dec1a6aeb4b5cc9c89c49e35d557db6a9ef3a41b
kerimKerimKerimKerimkerim <kerimkeirm7@gmail.com>
2025-09-06 15:00:51 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import styles from './index.module.css' // CSS'i bu şekilde ayıracağız
 
 export default function Home() {
   return (
@@ -10,11 +11,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="ez" />
+      <main style={{ textAlign: 'center', padding: '4rem 1rem' }}>
+        <Header title="Dowlander" />
+
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+
+        <div className={styles.downloadSection}>
+          <h2 className={styles.downloadText}>Download</h2>
+
+          <a href="/download.zip" className={styles.downloadButton}>
+            Download
+          </a>
+        </div>
       </main>
 
       <Footer />
